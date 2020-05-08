65ef3f9a5837ee1733241a159177a94ead391abd
Naoki Tomita <tomita@cpan.org>
2020-05-08 15:49:11 +0900

    うあああ (#1)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tomill/uaaa
+
+go 1.14
+
+require github.com/woothee/woothee-go v1.10.0 // indirect
--- main.go
+++ main.go
@@ -9,17 +9,29 @@ import (
 	"github.com/woothee/woothee-go"
 )
 
+type Log struct {
+	UserAgent string          `json:"ua"`
+	Result    *woothee.Result `json:"result"`
+	Error     string          `json:"error"`
+}
+
 func main() {
 	stdin := bufio.NewScanner(os.Stdin)
+	enc := json.NewEncoder(os.Stdout)
+
 	for stdin.Scan() {
 		ua := stdin.Text()
-
 		r, err := woothee.Parse(ua)
+
+		log := Log{
+			UserAgent: ua,
+			Result:    r,
+		}
 		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, err)
+			log.Error = err.Error()
 		}
 
-		if err := json.NewEncoder(os.Stdout).Encode(r); err != nil {
+		if err := enc.Encode(log); err != nil {
 			_, _ = fmt.Fprintln(os.Stderr, err)
 		}
 	}
